01df4c0dd0ae4d132a8b9d86e976ffe4659ed6a0
Mukhriddin Shakhriyorov <71601329+mshakhriyorov@users.noreply.github.com>
2021-08-26 17:24:05 +0500

    Final commit

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,21 +1,24 @@
-import React, { useEffect } from 'react';
-import useStorage from '../hooks/useStorage';
-
-
+import React, { useEffect } from "react";
+import useStorage from "../hooks/useStorage";
+import { motion } from "framer-motion";
 
 const ProgressBar = ({ file, setFile }) => {
-    const { progress, url } = useStorage(file);
-    console.log(progress, url);
+  const { progress, url } = useStorage(file);
+  console.log(progress, url);
 
-    useEffect(() => {
-        if (url) {
-            setFile(null);
-        }
-    }, [url, setFile])
+  useEffect(() => {
+    if (url) {
+      setFile(null);
+    }
+  }, [url, setFile]);
 
-    return (
-        <div className="progress-bar" style={{ width: progress + '%' }}></div>
-    )
-}
+  return (
+    <motion.div
+      className="progress-bar"
+      initial={{ width: 0 }}
+      animate={{ width: progress + "%" }}
+    ></motion.div>
+  );
+};
 
 export default ProgressBar;
